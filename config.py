18de4186a4e62b045a21cc3345b5f9afc5c58904
vasnu <154647578+vasnu@users.noreply.github.com>
2024-01-09 05:03:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DATABASE_URI = os.environ.get('DATABASE_URI', "")
+    DATABASE_URL = os.environ.get('DATABASE_URL', "")
     DATABASE_NAME = os.environ.get('DATABASE_NAME', "telebot")
